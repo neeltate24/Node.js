9dc45fd300ab2a89b0754602d9f0eb53539fa267
neeltate24 <neel.tate24@gmail.com>
2025-09-13 13:46:28 +0530

    enabled prettier formater

--- BE1.1_CW/models/twitterProfile.models.js
+++ BE1.1_CW/models/twitterProfile.models.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const twitterSchema = new mongoose.Schema({
-    profilePic: String,
-    fullName: String,
-    username: String,
-    bio: String,
-    companyName: String,
-    city: String,
-    portfolioLink: String,
-    handle: String,
-    followersCount: Number,
-    followingCount: Number,
-    isOnline: Boolean,
-})
+  profilePic: String,
+  fullName: String,
+  username: String,
+  bio: String,
+  companyName: String,
+  city: String,
+  portfolioLink: String,
+  handle: String,
+  followersCount: Number,
+  followingCount: Number,
+  isOnline: Boolean,
+});
 
-const Twitter = mongoose.model('Twitter', twitterSchema);
+const Twitter = mongoose.model("Twitter", twitterSchema);
 
 module.exports = Twitter;
